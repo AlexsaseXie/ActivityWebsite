72293bbd67390901279bb1337f90ddfe069cee8d
AlexsaseXie <thss15_xieyw@163.com>
2017-07-08 11:08:57 +0800

    Final7.7

--- activity_management/user_views.py
+++ activity_management/user_views.py
@@ -5,38 +5,11 @@ from .models import UserProfile, Activity, Join, Msg
 from django.utils import timezone
 from django.contrib import messages
 
-from .forms import ActivityForm,UploadImageForm
+from .forms import ActivityForm
 
 
 def show_user_info(request,user_id):
-    if request.method == 'POST':
-        form = UploadImageForm(request.POST)
-
-        if form.is_valid():
-            post = form.save(commit=False)
-
-            post.state = 1
-            post.user_id_id = request.user.id
-            post.priority = 0
-            post.want_to_join_count = 0
-            post.created_at = timezone.now()
-            post.save()
-            messages.info(request, '活动《{}》创建成功'.format(post.name))
-
-            print(post.id)
-            if UserProfile.check_user_can_join_activity(UserProfile(),request.user.id,post.id):
-                messages.info(request, '您已自动加入自己创建的活动《{}》'.format(post.name))
-                Join.create_join(Join(), user_id=request.user, activity_id=post, start_time=post.start_time,
-                             end_time=post.end_time, state=0)
-                post.want_to_join_count = 1
-                post.save()
-            else:
-                messages.info(request, '由于时间冲突，您未能加入自己创建的活动《{}》'.format(post.name))
-
-            form = ActivityForm()
-    else:
-        form = ActivityForm()
-
+    
     user = UserProfile.find_user_by_id(UserProfile(), user_id)
     return render(request, 'user_info.html', {'user_obj': user[0], 'user_profile': user[1]})
 
